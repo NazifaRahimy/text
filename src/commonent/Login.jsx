4ce01863f5c52662e29a87c427cd7cb52499cec5
nazifaRahimi <nazifarahimy6@gmail.com>
2025-03-04 11:19:00 -0800

    add section

--- src/commonent/Login.jsx
+++ src/commonent/Login.jsx
@@ -14,7 +14,17 @@ const Login = () => {
             The research around the impact of summer vacation is mixed.[1] "Summer learning loss" (or "summer slide")[2] is the perceived loss of learning students experience due to interrupted education, but the exact consequences of extended breaks is unclear. Some research highlights that school holidays can be stressful periods for children from low-income families.[3]
             </p>
         </div>
-     
+        <section>
+            <h1>this is fist tite</h1>
+            <p>The term summer vacation or summer break refers to a school break in the summer between school years and the break in the school academic year. Students are off anywhere between two weeks to three and a half months. Depending on the country and district, staff might be partially or fully excluded.
+
+            In Spain, Portugal, the Republic of Ireland, Italy, Greece, Georgia, Lithuania, Latvia, Lebanon, Romania and Russia, the summer break is normally three months, compared to two to six weeks (sometimes 3 months) in Australia, Brazil, the United Kingdom, Pakistan, Bangladesh, India, the Netherlands, Germany and Mexico.</p>
+            <h2>Criticisms and support</h2>
+            <hr />
+            <p>The research around the impact of summer vacation is mixed.[1] "Summer learning loss" (or "summer slide")[2] is the perceived loss of learning students experience due to interrupted education, but the exact consequences of extended breaks is unclear. Some research highlights that school holidays can be stressful periods for children from low-income families.[3]
+            The research around the impact of summer vacation is mixed.[1] "Summer learning loss" (or "summer slide")[2] is the perceived loss of learning students experience due to interrupted education, but the exact consequences of extended breaks is unclear. Some research highlights that school holidays can be stressful periods for children from low-income families.[3]
+            </p>
+        </section>
         
         </>
      );
